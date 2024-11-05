e51e29ca058828a38976b8a3a7dfa9a3e9febd81
junjin <junjin199655@gmail.com>
2024-11-04 23:40:18 -0500

    leetcode1

--- array/twosum1/Solution.java
+++ array/twosum1/Solution.java
@@ -1,4 +1,21 @@
 package array.twosum1;
-
+/*
+For this problem, we're going to use a HashMap. First of all, we need to traverse each nums[index].
+Then, if the map contains the complement, which is target - nums[index],
+it means there's a pair in the array that sums up to the target. If it doesn't,
+we store the current number and its index in the map to match up with the corresponding complements later.
+ */
 public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement)){
+                return new int[] { map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
 }
