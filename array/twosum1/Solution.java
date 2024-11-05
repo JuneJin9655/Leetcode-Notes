c28c2c73424c39977ab938849c4d0cfe01b6943b
junjin <junjin199655@gmail.com>
2024-11-04 23:44:54 -0500

    leetcode1

--- array/twosum1/Solution.java
+++ array/twosum1/Solution.java
@@ -4,6 +4,7 @@ For this problem, we're going to use a HashMap. First of all, we need to travers
 Then, if the map contains the complement, which is target - nums[index],
 it means there's a pair in the array that sums up to the target. If it doesn't,
 we store the current number and its index in the map to match up with the corresponding complements later.
+ time complex = O(n), for worst case, if hashmao doesnt find the two nums, then map need to store n Values
  */
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
